d22f59ca464470f3e2c8fffe2123a01aea4bb7ed
John McKie <therealjmac2097@gmail.com>
2017-05-23 11:28:07 +0100

    coding challenge completed

--- script.js
+++ script.js
@@ -97,5 +97,82 @@ if(23 == '23') {
 
 // Lecture: Boolean logic and Switch
 
+/*
+var age = 25;
+
+if(age < 20) {
+    console.log('John is a teenager');
+} else if(age >= 20 && age < 30) {
+    console.log('John is a young man');
+} else {
+    console.log('John is a man');
+}
 
 
+var job = 'teacher';
+
+job = prompt('What does John do?');
+
+switch (job) {
+    case 'teacher':
+    console.log('John teaches kids');
+    break;
+    case 'driver':
+    console.log('John drives a cab');
+    break;
+    case 'cop':
+    console.log('John fights crime');
+    break;
+    default:
+    console.log('John does something else');
+}
+
+*/
+
+//Coding challenge
+/*
+var johnHeight = 10;
+var derekHeight = 10;
+var johnAge = 39;
+var derekAge = 39;
+
+var johnTotal = johnHeight + (johnAge * 5);
+//console.log(johnTotal);
+var derekTotal = derekHeight + (derekAge * 5);
+//console.log(derekTotal);
+
+if(johnTotal > derekTotal) {
+    console.log('John is the winner with a score of ' + johnTotal);
+} else if(johnTotal < derekTotal) {
+    console.log('Derek is the winner with a score of ' + derekTotal);
+} else {
+    console.log('It was a draw');
+}
+*/
+/*
+// Extra challenge
+var johnHeight = 100;
+var derekHeight = 10;
+var janeHeight = 30;
+var johnAge = 39;
+var derekAge = 39;
+var janeAge = 23;
+
+var johnTotal = johnHeight + (johnAge * 5);
+console.log(johnTotal);
+var derekTotal = derekHeight + (derekAge * 5);
+console.log(derekTotal);
+var janeTotal = janeHeight + (janeAge * 5);
+console.log(janeTotal);
+
+if(johnTotal > derekTotal && johnTotal > janeTotal) {
+    console.log('John is the winner with a score of ' + johnTotal);
+} else if(derekTotal > johnTotal && derekTotal > janeTotal) {
+    console.log('Derek is the winner with a score of ' + derekTotal);
+} else if(janeTotal > johnTotal && janeTotal > derekTotal) {
+    console.log('Jane is the winner with a score of ' + janeTotal)
+} else {
+    console.log('It was a three way  draw');
+}
+
+*/
