9dcf432e25ce7c7b461cb7f5c819abfc8ad8aefe
John McKie <therealjmac2097@gmail.com>
2017-05-23 10:36:54 +0100

    If else statements

--- script.js
+++ script.js
@@ -70,6 +70,32 @@ console.log(ageOfMark);
 
 */
 
+/*
 // Lecture: if/else statements
+var name = 'John';
+var age = 26;
+var isMarried = 'yes';
+
+if(isMarried === 'yes') {
+    console.log(name + ' is married');
+} else {
+    console.log(name + ' will hopefully marry soon :)');
+}
+
+isMarried = false;
+
+if(isMarried){
+    console.log('Yes!');
+} else {
+    console.log('No!');
+}
+
+if(23 == '23') {
+    console.log('Something to print');
+}
+*/
+
+// Lecture: Boolean logic and Switch
+
 
 
