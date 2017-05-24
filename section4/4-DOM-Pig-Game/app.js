f0041463c27e44bae19e982569347d21a7eb893a
John McKie <therealjmac2097@gmail.com>
2017-05-24 21:02:59 +0100

    Basic dice rolls in place

--- section4/4-DOM-Pig-Game/app.js
+++ section4/4-DOM-Pig-Game/app.js
@@ -8,18 +8,32 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice; //setter
+//document.querySelector('#current-' + activePlayer).textContent = dice; //setter
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent; //getter
-console.log(x);
+//var x = document.querySelector('#score-0').textContent; //getter
+//console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    //display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+ dice + '.png';
+    
+    //update round score but only if the rolled number isnt a 1
+
+});
